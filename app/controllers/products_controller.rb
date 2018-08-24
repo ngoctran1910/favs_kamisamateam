6b38b073e626c03111e95a3c2b4a394f9326722b
Le Tat Tung <letattungtb@gmail.com>
2018-08-24 09:50:17 +0700

    add category in products

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,9 +1,15 @@
 class ProductsController < ApplicationController
   before_action :find_product, only: [:show]
+  before_action :find_category, only: [:index]
 
   def index
-    @products = Product.paginate(page: params[:page],
+    if params[:category_id]
+      @products = @category.products.paginate(page: params[:page],
       per_page: Settings.page_product_size).order(id: :asc)
+    else
+      @products = Product.paginate(page: params[:page],
+      per_page: Settings.page_product_size).order(id: :asc)
+    end
   end
 
   def show
@@ -16,6 +22,14 @@ class ProductsController < ApplicationController
     unless @product
       flash[:danger] = t "not_found.product"
       redirect_to not_found_index_path
+    end
+  end
+
+  def find_category
+
+    @category = Category.find(params[:category_id]) if params[:category_id]
+    unless @category
+      flash[:danger] = t "not_found.category"
     end
   end
 end
